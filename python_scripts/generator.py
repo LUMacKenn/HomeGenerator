987789f39a221225a0ef6cbab97c485ea59c341c
Mac Bross <bross.mackenn@gmail.com>
2020-12-10 03:38:43 -0500

    started code to try to distribute tiles more evenly

--- python_scripts/generator.py
+++ python_scripts/generator.py
@@ -1,4 +1,5 @@
 from ortools.sat.python import cp_model
+from random import randrange
 import json
 
 from tile_info import create_adjacency_mappings
@@ -27,7 +28,6 @@ for i in range(width):
         num_tiles_in_spot = sum([grid[i,j,k] for k in range(num_tiles)])
         model.Add(num_tiles_in_spot == 1)
 
-# nl = []
 # All adjacent tile constraints
 for i in range(width):
     for j in range(height):
@@ -38,11 +38,25 @@ for i in range(width):
                 if i2 >= 0 and i2 < width and j2 >= 0 and j2 < height:
                     model.AddBoolOr([grid[i2, j2, k2] for k2 in adjacency_mappings[k]["neighbors"][index]]).OnlyEnforceIf(grid[i, j, k])
 
+# Trying to make the layouts more diverse, not really working yet
+# type_of_each = []
+for k in range(num_tiles):
+    num_occurances_of_type = sum(grid[i,j,k]
+        for i in range(width)
+        for j in range(height)
+    )
+    # type_of_each.append(num_occurances_of_type)
+    model.Add(num_occurances_of_type <= 40)
+# avg = sum(type_of_each)/len(type_of_each)
+# avg = model.NewIntVar(0, width * height, "")
+# sum1 = sum(type_of_each)
+# len1 = len(type_of_each)
+# model.AddDivisionEquality(avg, sum1, len1)
+# deltas = [abs(val - avg) for val in type_of_each]
+# model.Minimize(sum(deltas))
+
 # To break symmetry
-# model.Add(grid[0,0,4] == True)
-# model.Add(grid[1,9,5] == True)
-# model.Add(grid[9,9,0] == True)
-model.Add(grid[0,0,13] == True)
+model.AddHint(grid[randrange(0, width), randrange(0, height), randrange(0, num_tiles)], True)
 
 if solver.Solve(model) in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
     # file = open("../Assets/Layouts/layout.txt", "w")
